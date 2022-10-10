9d05acd91bc658bd1dd6c444fec80fd150b47405
JerzyMarczewski <jerzy.marczewski2@gmail.com>
2022-10-11 00:00:47 +0200

    implemented password hashing

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const bcrypt = require("bcrypt");
+const app = express();
+
+app.use(express.json());
+
+let users = [{name: "john"}, {name: "emma"}]; 
+
+app.get("/users", (req, res) => {
+    res.json(users);
+});
+
+app.post("/users", async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const user = {name: req.body.name, password: hashedPassword};
+        res.status(200).send(user);
+    } catch {
+        res.status(500).send();
+    }
+});
+
+app.listen(5000);
