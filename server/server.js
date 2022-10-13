c694de1e9076c5aa20ccd5d790784cfe91e1739c
JerzyMarczewski <jerzy.marczewski2@gmail.com>
2022-10-13 10:30:54 +0200

    deleted unwanted code:

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 const express = require("express")
-const bcrypt = require("bcrypt");
 const app = express();
+const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
+const User = require("./models/User");
+
+
 
 app.use(express.json());
 
@@ -10,14 +14,28 @@ app.get("/users", (req, res) => {
     res.send(users);
 });
 
-app.post("/users", async (req, res) => {
-    try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-        const user = {name: req.body.name, password: hashedPassword};
-        res.status(200).send(user);
-    } catch {
-        res.status(500).send();
-    }
-});
+// app.post("/users", async (req, res) => {
+//     try {
+//         const hashedPassword = await bcrypt.hash(req.body.password, 10);
+//         const user = {name: req.body.name, password: hashedPassword};
+//         res.status(200).send(user);
+//     } catch {
+//         res.status(500).send();
+//     }
+// });
+
+// mongoose.connect("mongodb://localhost:27017/MERN-TODO",{ useNewUrlParser: true })
+//     .then(() => console.log("Connected to mongo"))
+//     .then(() => {
+//         const newUser = new User({
+//             name: "jan",
+//             email: "jan@mail.com",
+//             password: "jan1"
+//           });
+
+//         newUser.save();
+//     })
+
+
 
 app.listen(5000);
