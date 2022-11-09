17039c9feaf03b92cd2f2c91c29505daa42822c0
JerzyMarczewski <jerzy.marczewski2@gmail.com>
2022-11-09 10:39:44 +0100

    updates to mobile layout

---
+++ client/src/components/ListMenuButton/ListMenuButton.jsx
@@ -0,0 +1,22 @@
+import styles from "./ListMenuButton.module.css";
+import { Icon } from '@iconify/react';
+
+
+const ListMenuButton = (props) => {
+  return (
+    <div className={styles.container}>
+        <div className={styles.listName}>
+              <div className={styles.listIcon} style={{backgroundColor: `#${props.list.color}`}}>
+                <Icon icon="ant-design:unordered-list-outlined"/>
+              </div>
+              <div>{props.list.name}</div>
+            </div>
+            <div className={styles.numberOfItemsButton}>
+              <div>{props.list.items.filter(item => item.done === false).length}</div>
+              <Icon icon="ic:round-greater-than" width="16" />
+        </div>
+    </div>
+  )
+}
+
+export default ListMenuButton;
--- client/src/pages/Main/Main.jsx
+++ client/src/pages/Main/Main.jsx
@@ -2,6 +2,54 @@ import { useContext, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { UserContext } from "../../Context/UserContext";
 import styles from "./Main.module.css";
+import { Icon } from '@iconify/react';
+import ListMenuButton from "../../components/ListMenuButton/ListMenuButton";
+
+// sample list
+let lists = [
+  {
+    name: "list 1",
+    color: "04A5AA",
+    items: [
+        {
+            name: "item 1",
+            date: null,
+            done: false
+        },
+        {
+            name: "item 2",
+            date: null,
+            done: false
+        },
+        {
+          name: "item 3",
+          date: null,
+          done: true
+      }
+    ]
+  },
+  {
+    name: "list 2",
+    color: "DB162F",
+    items: [
+        {
+            name: "item 1",
+            date: null,
+            done: false
+        }
+    ]
+  },
+  {
+    name: "list 3",
+    color: "DFD630",
+    items: []
+  },
+  {
+    name: "list 4",
+    color: "09588E",
+    items: []
+  }
+]
 
 const Main = (props) => {
   const navigate = useNavigate();
@@ -15,24 +63,21 @@ const Main = (props) => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.header}>
-        <div className={styles.hamburger}>Hamburger</div>
-        <div>Profile</div>
-        <Link to="/login" onClick={() => setValue("")}>Logout</Link>
+      <div className={styles.navbar}>
+        <h4 className={styles.logo}>MERN-REMINDERS</h4>
+        <Icon icon="mi:options-horizontal" inline={true} />
       </div>
-      <div className={styles.listsContainer}>
-        <div className={styles.lists}>
-          <p>a</p>
-          <p>a</p>
-          <p>a</p>
-          <p>a</p>
+      <div className={styles.searchbar}>
+        <Icon icon="bx:search-alt-2" inline={true} />
+        <div>Search</div>
+      </div>
+      <div className={styles.listsMenu}>
+        <p className={styles.myLists}>My lists</p>
+        <div className={styles.listsContainer}>
+          {lists.map(list => <ListMenuButton list={list}/>)}
         </div>
-        {/* <div className={styles.listItems}>
-          <li>do something</li>
-          <li>do something</li>
-          <li>do something</li>
-        </div> */}
       </div>
+      <div className={styles.addListButton}>add list</div>
     </div>
   )
 }
