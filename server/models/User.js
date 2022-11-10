2174543f2f46b8dacb16d4cdbf349f729b2a84a7
JerzyMarczewski <jerzy.marczewski2@gmail.com>
2022-11-10 18:48:55 +0100

    updated the User Schema

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,35 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
+
+const ItemSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: null
+    },
+    done: {
+        type: Boolean,
+        default: false
+    }
+});
+
+const ListSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    // ! length must be 6
+    color: {
+        type: String,
+        required: true
+    },
+    items: [ItemSchema]
+});
+
 const UserSchema = new mongoose.Schema({
     username: {
         type: String,
@@ -10,7 +39,8 @@ const UserSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true
-    }
+    },
+    lists: [ListSchema]
 });
 
 module.exports = User = mongoose.model("users", UserSchema);
