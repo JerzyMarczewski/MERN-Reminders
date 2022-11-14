dfd3b656f12d63585f1ab809f96c4244b7c1c48c
JerzyMarczewski <jerzy.marczewski2@gmail.com>
2022-11-14 19:17:46 +0100

    added selectable colors and icons to the new list popup

--- client/src/components/AddList/AddList.jsx
+++ client/src/components/AddList/AddList.jsx
@@ -6,10 +6,26 @@ import styles from "./AddList.module.css";
 
  
 const COLORS = ["45B3E7","0078ff","bd00ff","ff9a00","01ff1f","e3ff00"]
+const ICONS = [
+    "ic:round-format-list-bulleted", 
+    "ic:round-school",
+    "ic:sharp-cake",
+    "ic:baseline-bookmark",
+    "ic:baseline-videogame-asset",
+    "ic:round-shopping-cart",
+    "ic:round-tv",
+    "ic:round-message",
+    "ic:round-shopping-bag",
+    "ic:round-sports-handball",
+    "ic:round-airplanemode-active",
+    "ic:round-backpack"
+
+]
 
 const AddList = (props) => {
     const [inputValue, setInputValue] = useState("");
-    const [selectedColor, setSelectedColor] = useState("45B3E7");
+    const [selectedColor, setSelectedColor] = useState(COLORS[0]);
+    const [selectedIcon, setSelectedIcon] = useState(ICONS[0]);
     const [isMounted, setIsMounted] = props.mounted;
     
 
@@ -27,11 +43,11 @@ const AddList = (props) => {
                         }
                     }>Cancel</div>
                     <h3>New list</h3>
-                    <div>OK</div>
+                    <div style={inputValue ? {} : {color: 'grey'}}>OK</div>
                 </div>
                 <div className={styles.listPreview}>
                     <div className={styles.listIcon} style={{backgroundColor: `#${selectedColor}`}}>
-                        <Icon icon="ant-design:unordered-list-outlined" width="80"/>
+                        <Icon icon={selectedIcon} width="80"/>
                     </div>
                         <div className={styles.listName}>
                             <input 
@@ -40,6 +56,7 @@ const AddList = (props) => {
                                 style={{color: `#${selectedColor}`}}
                                 value={inputValue} 
                                 onChange={(e) => setInputValue(e.target.value)} 
+                                autoFocus
                             />
                     </div>
                 </div>
@@ -51,6 +68,19 @@ const AddList = (props) => {
                             style={color === selectedColor ? {backgroundColor: `#${color}`, borderWidth: "2px"} : {backgroundColor: `#${color}`}}
                             onClick={() => setSelectedColor(color)}
                         />)
+                }</div>
+                <div className={styles.iconSelect}>{
+                    ICONS.map((icon) => 
+                        <div 
+                            key={icon}    
+                            className={styles.iconCircle}  
+                            style={icon === selectedIcon ? {borderWidth: "2px"} : {}}
+                            onClick={() => setSelectedIcon(icon)}
+                        >
+                            <Icon icon={icon} />
+                        </div>
+                        
+                    )
                 }</div>
             </div>
     )
