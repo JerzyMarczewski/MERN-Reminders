aa1c26fff8d04ed3a0c6118f7e4344b6462f72f3
JerzyMarczewski <jerzy.marczewski2@gmail.com>
2022-11-21 19:31:32 +0100

    created and finished FetchContext

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,28 @@
 import {useState, useEffect} from "react";
 import {Route, Routes, BrowserRouter, Navigate} from "react-router-dom";
-
 import './App.css';
 import Main from "./pages/Main/Main";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import {UserContext} from "./Context/UserContext";
+import {FetchContext} from "./Context/FetchContext";
 
 function App() {
   const [value, setValue] = useState(""); 
+  const [fetchIteration, setFetchIteration] = useState(0);
 
   return (
     <div className="App">
 
      <BrowserRouter>
       <UserContext.Provider value={{value, setValue}}>
-        <Routes>
-          <Route exact path="/" element={<Main />}/>
-          <Route path="/login" element={<Login cb={(uname) => setValue(uname)} />}/>
-          <Route path="/register" element={<Register />} />
-        </Routes>
+        <FetchContext.Provider value={{fetchIteration, setFetchIteration}}>
+          <Routes>
+            <Route exact path="/" element={<Main />}/>
+            <Route path="/login" element={<Login cb={(uname) => setValue(uname)} />}/>
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </FetchContext.Provider>
       </UserContext.Provider>
      </BrowserRouter>
     </div>
