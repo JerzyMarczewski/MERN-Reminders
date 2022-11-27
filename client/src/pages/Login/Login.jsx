4a7da8b0c7bdb3e148af380b75c5b743396bc306
JerzyMarczewski <jerzy.marczewski2@gmail.com>
2022-11-27 23:02:27 +0100

    styled login page

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,16 +1,17 @@
-import {Link, useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useEffect, useContext, useState } from "react";
 import { UserContext } from "../../Context/UserContext";
 import axios from "axios";
+import styles from "./Login.module.css";
+import { Icon } from "@iconify/react";
 
 const Login = (props) => {
+  const navigate = useNavigate();
 
-  const navigate = useNavigate()
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  const [username, setUsername] = useState(""); 
-  const [password, setPassword] = useState(""); 
-
-  const {value, setValue} = useContext(UserContext);
+  const { value, setValue } = useContext(UserContext);
 
   useEffect(() => {
     if (value) navigate("/");
@@ -22,37 +23,62 @@ const Login = (props) => {
     if (!username) return alert("Username is required");
     if (!password) return alert("Password is required");
 
-    axios.post("http://localhost:5000/login", {
-      username: username,
-      password: password
-    }).then(res => {
-      console.log(res.data);
-      if (!res.data.ok) return alert(res.data.message);
+    axios
+      .post("http://localhost:5000/login", {
+        username: username,
+        password: password,
+      })
+      .then((res) => {
+        console.log(res.data);
+        if (!res.data.ok) return alert(res.data.message);
 
-      setValue(username.toLowerCase());
-      navigate("/");
-    }).catch(err => console.log(err));
-
-    
-  }
+        setValue(username.toLowerCase());
+        navigate("/");
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
-    <>
-        <h1>Login</h1>
-        <form onSubmit={handleLoginSubmit}>
-            <div>
-                <label htmlFor="username">Username</label>
-                <input type="username" id="username" name="username" onChange={(e) => setUsername(e.target.value)} />
-            </div>
-            <div>
-                <label htmlFor="password">Password</label>
-                <input type="text" id="password" name="password" onChange={(e) => setPassword(e.target.value)} />
-            </div>
-            <button type="submit">Login</button>
+    <div className={styles.container}>
+      <div className={styles.loginBox}>
+        <h1>Log in</h1>
+        <form className={styles.loginForm} onSubmit={handleLoginSubmit}>
+          <div className={styles.field}>
+            <Icon icon="mdi:user-circle" color="ffffff" width="32" />
+            <input
+              type="text"
+              id="Username"
+              name="Username"
+              placeholder="Username"
+              spellCheck={false}
+              autoComplete="off"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className={styles.field}>
+            <Icon icon="mdi:password-alert" color="ffffff" width="32" />
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Password"
+              spellCheck={false}
+              autoComplete="off"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button type="submit" className={styles.submitButton}>
+            <h2>Log in</h2>
+          </button>
         </form>
-        <Link to="/register">Register</Link>
-    </>
-  )
-}
+        <p className={styles.bottomText}>
+          Don't have an account? <Link to="/register">Register</Link>
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
