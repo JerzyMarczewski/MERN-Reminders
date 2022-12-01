8de8c405eb0f9b357f8bfef3996ce39a76ba8296
JerzyMarczewski <jerzy.marczewski2@gmail.com>
2022-12-01 12:23:34 +0100

    created and finished UserDropdown component

---
+++ client/src/components/UserDropdown/UserDropdown.jsx
@@ -0,0 +1,50 @@
+import styles from "./UserDropdown.module.css";
+import { Icon } from "@iconify/react";
+import { useContext, useState } from "react";
+import { UserContext } from "../../Context/UserContext";
+import { useNavigate } from "react-router-dom";
+
+const UserDropdown = () => {
+  const navigate = useNavigate();
+  const { setValue } = useContext(UserContext);
+
+  const [dropdownShown, setDropdownShown] = useState(false);
+
+  const handleLogout = () => {
+    setValue("");
+    navigate("/login");
+  };
+
+  if (!dropdownShown)
+    return (
+      <div className={styles.options} onClick={() => setDropdownShown(true)}>
+        <div className={styles.userButton}>
+          <h4>User</h4>
+          <Icon icon="gridicons:dropdown" />
+        </div>
+      </div>
+    );
+
+  return (
+    <>
+      <div className={styles.user}>
+        <div className={styles.userButton}>
+          <h4>User</h4>
+          <Icon icon="gridicons:dropdown" />
+        </div>
+        <div className={styles.dropdown}>
+          <div className={styles.dropdownItem} onClick={handleLogout}>
+            <div>Logout</div>
+            <Icon icon="material-symbols:logout-rounded" />
+          </div>
+        </div>
+        <div
+          className={styles.background}
+          onClick={() => setDropdownShown(false)}
+        ></div>
+      </div>
+    </>
+  );
+};
+
+export default UserDropdown;
